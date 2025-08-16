af22fe7128e9522b4ba6f8126930f27cc42da634
AL HOSSAIN ABID <109515301+gLaaSSb00m@users.noreply.github.com>
2025-08-16 18:05:28 +0600

    Update wsgi.py

--- rice_prediction/wsgi.py
+++ rice_prediction/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rice_prediction.settings')
 
 application = get_wsgi_application()
+import tensorflow as tf
+tf.config.threading.set_intra_op_parallelism_threads(2)
+tf.config.threading.set_inter_op_parallelism_threads(2)
